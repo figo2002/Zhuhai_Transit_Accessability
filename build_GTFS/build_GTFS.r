4a422287b100b3540a8004245584f6715684ba7e
awu <fanw86@gmail.com>
2018-03-29 17:17:18 +0800

    gtfs stuff

--- build_GTFS/build_GTFS.r
+++ build_GTFS/build_GTFS.r
@@ -12,15 +12,23 @@ library(stringdist)
 library(data.table)
 library(fasttime)
 
-jsonfile <- paste0(getwd(),"/osm_bus_stops_zhuhai.geojson")
-
+jsonfile <- "osm_bus_stops_zhuhai.geojson"
 
 
 stops<- readOGR(jsonfile, require_geomType="wkbPoint",use_iconv = TRUE, encoding = "UTF-8")
 
+# read stations defined by polygons
 stations <- readOGR(jsonfile,  require_geomType="wkbPolygon",use_iconv = TRUE, encoding = "UTF-8")
-
-stations_p <- SpatialPointsDataFrame(gCentroid(stations, byid=TRUE), 
+# convert station polygons to points
+stations <- SpatialPointsDataFrame(gCentroid(stations, byid=TRUE), 
                                      stations@data, match.ID=FALSE)
-tem=rbind(stops,stations_p)
-kk=as.data.frame(rbind(stops,stations_p))
+
+all_stops=rbind(stops,stations)
+#all_stops=as.data.frame(rbind(stops,stations))
+
+
+noname_stops <- all_stops[is.na(all_stops$name),]
+  
+named_stops <- all_stops[!is.na(all_stops$name),]
+
+
